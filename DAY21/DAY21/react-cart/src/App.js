28490f8f453a34421c254341de9ec06981f260ae
priyadh1712 <prixaashok2003@gmail.com>
2024-09-28 16:41:43 +0530

    completed day21 task

--- DAY21/DAY21/react-cart/src/App.js
+++ DAY21/DAY21/react-cart/src/App.js
@@ -7,6 +7,10 @@ const productList = [
   { id: 2, name: 'Special Item', price: '$18.00', description: 'Special item description' },
   { id: 3, name: 'Sale Item', price: '$25.00', description: 'Sale item description' },
   { id: 4, name: 'Popular Item', price: '$40.00', description: 'Popular item description' },
+  { id: 5, name: 'Product 5', price: '$40.00 - $80.00', description: 'Product description' },
+  { id: 6, name: 'Product 6', price: '$18.00', description: 'Product description' },
+  { id: 7, name: 'Product 7', price: '$25.00', description: 'Product description' },
+  { id: 8, name: 'Product 8', price: '$40.00', description: 'Product description' },
 ];
 
 function App() {
@@ -24,13 +28,41 @@ function App() {
 
   return (
     <div className="container">
-      <header className="d-flex justify-content-between align-items-center py-3">
-        
-        <button className="btn btn-dark">
-          Cart <span className="badge bg-light text-dark">{cart.length}</span>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">React Cards</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
         </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Shop</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <button className="btn btn-dark">
+            Cart <span className="badge bg-light text-dark">{cart.length}</span>
+          </button>
+        </div>
+      </nav>
+
+
+      <header className="bg-dark py-5">
+        <div className="container px-4 px-lg-5 my-5">
+          <div className="text-center text-white">
+            <h1 className="display-4 fw-bolder">Shop in style</h1>
+            <p className="lead fw-normal text-white-50 mb-0">With this shop homepage template</p>
+          </div>
+        </div>
       </header>
-      <h1>Shop in style</h1>
+      <br></br>
       <div className="row">
         {productList.map(product => (
           <div key={product.id} className="col-md-3 mb-4">
